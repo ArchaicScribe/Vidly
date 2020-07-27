a5c145ec9036562c82467883f1ce13cdb8762885
Alex Rauenzahn <xandermv@yahoo.com>
2020-07-27 11:05:55 -0600

    Added in RandomMovieViewModel, created new folder, and add Customer class.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,56 +5,37 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
 
-        
+
         // GET: Movies
         public ActionResult Random()
         {
 
-
-
             var movie = new Movie() { Name = "Shrek!" };
-
-            //return View(movie);
-
-            //return new ViewResult();
-
-            //return Content("Hello World!");
-
-            //return HttpNotFound();
-
-            //return new EmptyResult();
-
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }
-        [Route("movies/released{year}/{monthj:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
+            var customers = new List<Customer>
             {
-                pageIndex = 1;
-            }
-            if (string.IsNullOrWhiteSpace(sortBy))
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
             {
-                sortBy = "Name";
-            }
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+
+
+
+
         }
-        
     }
 }
